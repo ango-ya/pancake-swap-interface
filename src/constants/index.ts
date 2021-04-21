52e35e5ce75e7cf4fcac55adb06e36445e943e2d
seki <schokolade0317@gmail.com>
2021-04-21 16:33:27 +0900

    use our contract

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
 
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0xFD590adc2De440D0B0109C4adaa4a604E90cAC84'
 
 // a list of tokens by chain
 type ChainTokenList = {
--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -87,9 +87,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0xBCfCcbde45cE874adCB698cC183deBcF17952812', // v2 factory
+  '0xA348C9690A81C9197eDBb36aCfB9e143625a49b8', // v2 factory
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F', // v2 router 02
+  '0xFD590adc2De440D0B0109C4adaa4a604E90cAC84', // v2 router 02
 ]
 
 /**
